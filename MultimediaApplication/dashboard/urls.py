c196a5b87b8024b0705a5bc2218525caf7f53969
NineQueen <54728702+NineQueen@users.noreply.github.com>
2025-06-11 14:06:16 +0800

    Add files via upload

--- MultimediaApplication/dashboard/urls.py
+++ MultimediaApplication/dashboard/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('temp_data', views.temp_data, name = 'temp_data'),
-    path("classroom",views.class_index)
+    path("classroom",views.class_index),
+    path("history",views.history_index),
 ]
